1b6f6da64b4ec2e6b5bd1485b9173664c64565a1
toetoeaungg <toetoeaungg@outlook.com>
2024-09-28 11:21:27 +0630

    change master.sh

--- kubeadm_1.30/scripts/master.sh
+++ kubeadm_1.30/scripts/master.sh
@@ -7,31 +7,7 @@ set -euxo pipefail
 
 # If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
-PUBLIC_IP_ACCESS="false"
-IPADDR="192.168.56.2"
-NODENAME=$(hostname -s)
-POD_CIDR="10.244.0.0/16"
-
-# Pull required images
-
-#sudo kubeadm config images pull
-
-# Initialize kubeadm based on PUBLIC_IP_ACCESS
-
-if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
-    
-    MASTER_PRIVATE_IP=$(ip addr show eth1 | awk '/inet / {print $2}' | cut -d/ -f1)
-    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --cri-socket unix:///var/run/containerd/containerd.sock --ignore-preflight-errors Swap
-
-elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
-
-    MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
-    sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
-
-else
-    echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
-    exit 1
-fi
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.56.2 --cri-socket unix:///var/run/containerd/containerd.sock
 
 # Configure kubeconfig
 
